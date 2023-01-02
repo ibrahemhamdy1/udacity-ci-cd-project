bacd235f15c8f15dd75185ff25b1d79e276ad1dc
Ibrahim hamdy <mslm356@outlook.com>
2023-01-02 10:20:47 +0200

    Add console log in main.js in the backend.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,8 @@ import { TransformInterceptor } from './modules/common/interceptors/TransformInt
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
+console.log('Ibrahim hamdy ramadan from udapeople');
+
 async function bootstrap() {
   const logger = new AppLogger();
   logger.info(`NodeJs Version ${process.version}`);
